4782eed1b112d4fd5185dc237090664e3ae8d7f7
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-03-28 00:49:16 +0300

    Styled footer.css

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -7,8 +7,9 @@ function Filter() {
   return (
     <div className="filter-div">
       {links.map((item, i) => (
-        <div key={i} className="links-bos">
-          <img src={item} alt="" />
+        <div key={i} className="links-box">
+          <img src={item.imgSrc} alt="" className='links-img' />
+          <p className='links-label'>{item.label}</p>
         </div>
       ))}
       ;
