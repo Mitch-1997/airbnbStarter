ea1981ad7bfe5f966a829f9615be9893798a24b1
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-03-27 17:16:29 +0300

    added Footer component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Header.css'
 import Logo from "../../assets/logo/long-logo.png"
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
+import BasicMenu from './ProfileMenu';
+import SimpleBottomNavigation from './BottomNav';
+import MobileSearchBar from '../MobileSearchBar/MobileSearchBar';
+
 
 function Header() {
   return (
@@ -10,8 +16,21 @@ function Header() {
         <div className="search-bar-text">Anywhere</div>
         <div className="search-bar-text">Any Week</div>
         <div className="search-bar-text2">Add Guests</div>
+        <div className="search-icon-div">
+          <SearchRoundedIcon className="search-icon" />
+        </div>
       </div>
-      <div className="profile-container"></div>
+      <div className="profile-container">
+        <div className="airbnb-your-home">Airbnb Your Home</div>
+        <div className="globe-div">
+          <LanguageRoundedIcon sx={{ fontSize: "1.3rem" }} />
+        </div>
+        <div className="profile-div">
+          <BasicMenu />
+        </div>
+      </div>
+      <MobileSearchBar />
+      <SimpleBottomNavigation />
     </div>
   );
 }
---
+++ src/Components/MobileSearchBar/MobileSearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MobileSearchBar.css"
+
+function MobileSearchBar() {
+  return (
+    <div className='mobile-search-bar'>MobileSearchBar</div>
+  )
+}
+
+export default MobileSearchBar
